15da903a04d59a879685c0e1589fb855c756a93f
vagrant <vagrant@vagrant.vm>
2018-04-05 02:37:01 +0000

    Created function to simplify file and corrected the variables

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,29 @@
-print "Day 1"
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print "Delivered {} {}s for total of ${}".format(
-        count, melon, amount)
-the_file.close()
+#Deliveries of melons
 
 
-print "Day 2"
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def deliveries(data_file):
+    """
+    Opens the file specified in the argument. It then prints out the number,
+    type, and cost of the melons delivered during certain days.
+    """
+    the_file = open(data_file)
+    if data_file == "um-deliveries-20140519.txt":
+        print "Day 1"
+    elif data_file == "um-deliveries-20140520.txt":
+        print "Day 2"
+    elif data_file == "um-deliveries-20140521.txt":
+        print "Day 3"
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+        melon = words[0]
+        count = words[-2]
+        amount = words[-1]
+        print "Delivered {} {}s for total of ${}".format(
+            count, melon, amount)
+    the_file.close()
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
 
-    print "Delivered {} {}s for total of ${}".format(
-        count, melon, amount)
-the_file.close()
-
-
-print "Day 3"
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print "Delivered {} {}s for total of ${}".format(
-        count, melon, amount)
-the_file.close()
+deliveries("um-deliveries-20140519.txt")
+deliveries("um-deliveries-20140520.txt")
+deliveries("um-deliveries-20140521.txt")
